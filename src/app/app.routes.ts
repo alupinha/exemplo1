1f1df13b43604520d5d1678d8fd786159f64bf19
Paula <paularoessle@gmail.com>
2025-11-27 22:22:30 +0000

    correção de componentes 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Exemplo1 } from './pages/exemplo1/exemplo1';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'', redirectTo:"welcome", pathMatch: 'full'},
+  {path:"welcome", loadComponent: () => import ('./pages/welcome/welcome').then(c => c.Welcome) },
+
+
+  /**Rota padrão sem laseyload */
+  {path:"pages/uc00604", component: Exemplo1},
+  {path:"pages/autenticacao", loadComponent: () => import('./pages/authentication/authentication').then(c => c.Authentication)},
+
+  {path:"**", loadComponent: () => import('./pages/welcome/welcome').then(c => c.Welcome)},
+];
