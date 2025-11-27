c5146ed10663d80679fb1111793e09a7caa199e5
Paula <paularoessle@gmail.com>
2025-11-27 22:16:36 +0000

    correção rotas

---
+++ src/app/pages/exemplo1/exemplo1.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exemplo1',
+  imports: [],
+  templateUrl: './exemplo1.html',
+  styleUrl: './exemplo1.css',
+})
+export class Exemplo1 {
+
+}
