5a315a93ac1f963f8c5caf5e7dc0bea6b5910109
Paula <paularoessle@gmail.com>
2026-01-08 23:24:33 +0000

    correcoes...

--- src/app/components/avatar/avatar.ts
+++ src/app/components/avatar/avatar.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-avatar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './avatar.html',
   styleUrl: './avatar.css',
 })
 export class Avatar {
 authGmailService: any;
-localUser: any;
+localUser: Observable <any>=of([]);
 
 }
