23030bf606fee2e6ed24f413a968918cc35b3904
Paula <paularoessle@gmail.com>
2026-01-08 23:05:37 +0000

    correcoes

---
+++ src/app/components/avatar/avatar.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-avatar',
+  imports: [],
+  templateUrl: './avatar.html',
+  styleUrl: './avatar.css',
+})
+export class Avatar {
+authGmailService: any;
+localUser: any;
+
+}
