4ab256b826f816bead0233d19b9295f50e87f9d7
Paula <paularoessle@gmail.com>
2026-01-08 22:33:11 +0000

    Correção do formulario

--- src/app/pages/authentication/authentication.ts
+++ src/app/pages/authentication/authentication.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-authentication',
@@ -14,8 +14,8 @@ export class Authentication {
 
    constructor(private fb: UntypedFormBuilder) {
        this.signinForms = this.fb.nonNullable.group({
-         email: [''],
-         password:['']
+         email: ['', [Validators.required, Validators.email]],
+         password:['', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]]
        });
 
      console.log('meu form', this.signinForms.value)
@@ -23,6 +23,9 @@ export class Authentication {
    }
 
 onSubmit() {
+  this.signinForms.get('email')?.getError('required');
+  console.log("Controls", this.signinForms.controls['email']);
+
 console.log("Meu Forms", this.signinForms.value);
 }
 
