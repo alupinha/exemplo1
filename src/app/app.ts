9efc26dd1dd330f0dbae5bed0ebdb208bae20954
Paula <paularoessle@gmail.com>
2026-01-15 21:08:35 +0000

    header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,16 @@
-import { RouterLink } from "@angular/router";
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Header } from "./componentes/header/header";
+import { Main } from "./componentes/main/main";
+import { Footer } from "./componentes/footer/footer";
 
-imports: [RouterLink]
 
+@Component({
+  selector: 'app-root',
+  imports: [Header, Main, Footer],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('Paula');
+}
