c2178bd998dd08f4fb896ed0a773dc4cd5628050
Paula <paularoessle@gmail.com>
2026-01-15 20:40:16 +0000

    correcao header

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,3 +1,10 @@
 import { RouterLink } from "@angular/router";
-
+import { Component } from "@angular/core";
  imports: [RouterLink]
+
+ @Component({
+  selector:'cabeçalho do aplicativo',
+  
+
+
+ })
